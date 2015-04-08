ed6b3d2fe939beee0e6762a68c0b8a3eccd59fc0
Boyan Wan <boyan.wan@gmail.com>
2015-04-08 17:02:20 +0800

    Add devise

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+	before_action :authenticate_user!, except: [:index, :show]
   
   def index
     @posts = Post.all.order('created_at DESC')
@@ -42,7 +43,7 @@ class PostsController < ApplicationController
 
   	redirect_to posts_path
   end
-  
+
   private
 
   def post_params
